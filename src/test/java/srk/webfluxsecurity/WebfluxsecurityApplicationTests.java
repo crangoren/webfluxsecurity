c625aba8959cb8c009c2582e3e999e4212b6e5ea
R.K.Suleimanov <crangoren@gmail.com>
2023-07-19 13:45:46 +0300

    Initial commit

---
+++ src/test/java/srk/webfluxsecurity/WebfluxsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package srk.webfluxsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
